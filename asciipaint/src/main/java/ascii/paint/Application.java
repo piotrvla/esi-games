1a34caf2aebe57b6a69b76ce6ca50033f3896d6e
56212 <56212@etu.he2b.be>
2021-10-03 18:12:03 +0200

    Adding missing javadoc, deleting useless methods and imrpving the code

--- asciipaint/src/main/java/ascii/paint/Application.java
+++ asciipaint/src/main/java/ascii/paint/Application.java
@@ -1,7 +1,6 @@
 package ascii.paint;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -11,28 +10,15 @@ public class Application {
 
     private AsciiPaint ascii;
 
-    /**
-     *
-     */
     public Application() {
         this.ascii = new AsciiPaint();
 
     }
 
-    /**
-     *
-     * @param width
-     * @param height
-     */
     public Application(int width, int height) {
         this.ascii = new AsciiPaint(width, height);
     }
 
-    /**
-     *
-     * @param message
-     * @return
-     */
     private static int readNumber(String message) {
         Scanner kbd = new Scanner(System.in);
         System.out.println(message);
@@ -44,27 +30,21 @@ public class Application {
         return kbd.nextInt();
     }
 
-    /**
-     *
-     * @param message
-     * @param min
-     * @param max
-     * @return
-     */
-    private static int sizeBetweenRange(String message, int min, int max) {
+    private static int sizeBetweenRange(String message, int min) {
 
         int number = readNumber(message);
-        while (number < min || (max < number && max != 0)) {
+        while (number < min) {
             System.out.println("Inserted number isn't above " + min);
             number = readNumber(message);
         }
         return number;
     }
 
-    /**
-     *
-     * @return
-     */
+    private Application createAsciiPaint() {
+        return new Application(sizeBetweenRange("width", 0),
+                sizeBetweenRange("height", 0));
+    }
+
     private static Application defaultSizeOrNot() {
         System.out.println("Are you willing to create a"
                 + " default size of the drawing?");
@@ -74,17 +54,12 @@ public class Application {
         } else {
             return new Application(
                     sizeBetweenRange("Insert the width,"
-                            + " it must be above 10", 10, 0),
+                            + " it must be above 10", 10),
                     sizeBetweenRange("Insert the height,"
-                            + " it must be above 10", 10, 0));
+                            + " it must be above 10", 10));
         }
     }
 
-    /**
-     *
-     * @param message
-     * @return
-     */
     private static boolean defaultOrNot(String message) {
         Scanner kbd = new Scanner(System.in);
         System.out.println(message);
@@ -95,122 +70,43 @@ public class Application {
         return kbd.nextLine().charAt(0) == 'y';
     }
 
-    /**
-     *
-     * @param command
-     */
-    private void initializeParams(String[] command) {
-        if (checkTypeOfParams(command) && checkValueOfParams(command)) {
-            switch (command[1].toLowerCase()) {
-                case "circle":
-                    this.ascii.newCircle(Integer.parseInt(command[2]),
-                            Integer.parseInt(command[3]),
-                            Double.parseDouble(command[4]),
-                            command[5].charAt(0));
-                    break;
-                case "rectangle":
-                    this.ascii.newRectangle(Integer.parseInt(command[2]),
-                            Integer.parseInt(command[3]),
-                            Double.parseDouble(command[4]),
-                            Double.parseDouble(command[5]),
-                            command[6].charAt(0));
-                    break;
-                case "square":
-                    this.ascii.newSquare(Integer.parseInt(command[2]),
-                            Integer.parseInt(command[3]),
-                            Double.parseDouble(command[4]),
-                            command[5].charAt(0));
-                    break;
-            }
-        } else {
-            System.err.println("Retry, the command looks like: "
-                    + "add [shape] [x] [y] [radius/width/height] [color] ");
-        }
-
-    }
-
-    /**
-     *
-     * @param command
-     * @return
-     */
-    private boolean checkTypeOfParams(String[] command) {
-        Pattern pattern = Pattern.compile("\\d+(\\.\\d+)?");
-        if ((command.length == 6 && !"rectangle".equals(command[1]))
-                || (command.length == 7 && "rectangle".equals(command[1]))) {
-
-            for (int i = 2; i < command.length - 1; i++) {
-                if (command[i] == null) {
-                    return false;
-                }
-                if (!pattern.matcher(command[i]).matches()) {
-                    return false;
-                }
-            }
-
-        } else {
-            return false;
-        }
+    private boolean checkParams(String[] command, int nbParam) {
 
         return true;
     }
 
-    private boolean checkValueOfParams(String[] command) {
-        int width;
-        int height;
-        if (command.length == 6) {
-            width = Integer.parseInt(command[4]);
-            height = Integer.parseInt(command[4]);
-        } else if (command.length == 7) {
-            width = Integer.parseInt(command[4]);
-            height = Integer.parseInt(command[5]);
-        } else {
-            return false;
-        }
-        if (Integer.parseInt(command[2]) >= this.ascii.getWidth()
-                || Integer.parseInt(command[2]) + width
-                >= this.ascii.getWidth()) {
-            return false;
-        }
-        if (Integer.parseInt(command[3]) > this.ascii.getHeight()
-                || Integer.parseInt(command[3]) + height
-                >= this.ascii.getWidth()) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     *
-     */
     public void start() {
         String[] command;
         Scanner kbd = new Scanner(System.in);
         boolean exit = false;
         while (!exit) {
-            System.out.println("Avaiable commands: add "
-                    + "(circle, rectangle, square), show, help and exit");
+            System.out.println("Avaiable commands: move, add "
+                    + "(circle, rectangle, square) and show");
             String input = kbd.nextLine();
             command = input.split(" ");
-            switch (command[0].toLowerCase()) {
+            switch (command[0]) {
                 case "add":
-                    initializeParams(command);
+                    if (command[1].toLowerCase() == "circle") {
+
+                        this.ascii.newCircle(Integer.parseInt(command[2]),
+                                Integer.parseInt(command[3]),
+                                Integer.parseInt(command[4]),
+                                command[5].charAt(0));
+                    }
                     break;
                 case "show":
-                    String ascii = this.ascii.asAscii();
-                    for (int i = 0; i < ascii.length(); i++) {
-                        System.out.printf("%2s", ascii.charAt(i));
-                    }
+                    System.out.printf(this.ascii.asAscii());
                     break;
                 case "exit":
                     exit = true;
                     break;
+                case "help":
+                    break;
                 default:
-                    System.err.println("Wrongly implemented command,"
-                            + " use command help");
                     break;
             }
         }
+
     }
 
     public static void main(String[] args) {
