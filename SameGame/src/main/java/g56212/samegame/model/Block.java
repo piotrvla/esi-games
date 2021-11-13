c44e6221e241b5cffaefab775098f8dab01328b2
56212 <56212@etu.he2b.be>
2021-11-13 21:56:28 +0100

    Javadoc of the block

--- SameGame/src/main/java/g56212/samegame/model/Block.java
+++ SameGame/src/main/java/g56212/samegame/model/Block.java
@@ -1,16 +1,37 @@
 package g56212.samegame.model;
 
+/**
+ * Block component that represents a colored block in the game.
+ */
 public class Block {
+
     private Color color;
 
+    /**
+     * Constructor of the block that needs a color to be initialised.
+     *
+     * @param color Color to initialize the block.
+     */
     public Block(Color color) {
         this.color = color;
     }
 
+    /**
+     * Getter of the block's color.
+     *
+     * @return Color of the block.
+     */
     public Color getColor() {
         return this.color;
     }
 
+    /**
+     * Equals method that compares 2 different blocks, by checking their colors that exists in COLOR enum.
+     * If the colors are the same true is returned, if not false.
+     *
+     * @param block An other block to compare with.
+     * @return Boolean
+     */
     public boolean equals(Block block) {
         if (block == null) {
             throw new IllegalArgumentException("Block is null");
