3a23962674ff646f81e358e1de6374e8ba6275ba
56212 <56212@etu.he2b.be>
2021-11-27 11:50:47 +0100

    Modifying pom.xml, adding main class of javafx

--- SameGame/src/main/java/g56212/samegame/SameGame.java
+++ SameGame/src/main/java/g56212/samegame/SameGame.java
@@ -1,9 +1,34 @@
 package g56212.samegame;
 
+import g56212.samegame.controller.ControllerFX;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
 /**
- * 
- * @author 56212
+ * Main class of the game Simon
+ *
+ * @author piotr
  */
-public class SameGame {
+public class SameGame extends Application {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+
+    /**
+     *
+     * @param stage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        ControllerFX controller = new ControllerFX();
+        controller.start(stage);
+    }
 
 }
--- SameGame/src/main/java/g56212/samegame/controller/ControllerFX.java
+++ SameGame/src/main/java/g56212/samegame/controller/ControllerFX.java
@@ -3,13 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package g56212.samegame.controller;
 
+import javafx.stage.Stage;
+
 /**
- * 
+ *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class ControllerFX {
 
+    public ControllerFX() {
+    }
+
+    public void start(Stage stage) {
+
+    }
 }
